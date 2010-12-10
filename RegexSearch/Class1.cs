e3d57ebe1db1b95b4425ff9ec0230b6c21386f0a
Andreas Ling <andreas.ling@gmail.com>
2010-12-10 17:12:17 +0100

    changes to testfile and added some notes

--- RegexSearch/Class1.cs
+++ RegexSearch/Class1.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RegexSearch
+namespace RegexSearch.TestCode
 {
     class Class1
     {
     }
 
-
-    class _my_123Class
+    class _my_123Class : MyInterface
     {
+        private System.Int32 myUnassignedField;
+
+        private int myAssignedField = 0;
+
+        int _my_123AlpfanumericField ;
+
+        public int MyProperty{ get; set; }
+
         public int _my_123Method(int _para123meter)
         {
-            System.Collections.Generic.List<System.String> _my_123Strings = null;
+            System.Collections.Generic.List<System.String> _my_123GenericListOfStrings = null;
 
             return _my_123Strings.Count();
         }
+
+        void myMethod () {
+
+            for (var iInFor = 0; iInFor < 10; iInFor++) { }
+
+            var linqResult =
+                from linqLocalVar in Enumerable.Empty<string>() select linqLocalVar;
+        }
+    }
+
+    interface MyInterface{
+        int InterfaceMethodWithParameter(int _para123meter);
+        int InterfaceProperty { get; set; }
     }
 }
